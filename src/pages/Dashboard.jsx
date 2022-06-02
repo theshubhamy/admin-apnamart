a30def2ca160e4e4e0ae77e79cece7ff5c9bc5bf
shubham kumar <devshubhamyadav@gmail.com>
2022-06-02 21:40:49 +0530

    complete

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,52 @@
 import React from "react";
-
+import Main from "../layout/Main";
+import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from "../assets/icons";
+import RoundIcon from "../components/RoundIcon";
+import InfoCard from "../components/card/InfoCard";
 const Dashboard = () => {
   return (
-    <div>
-      <div className="md:ml-64 bg-white">
-        <div className="px-4 md:px-10 mx-auto w-full">dashbiard</div>
-      </div>
-    </div>
+    <>
+      <Main>
+        <h1>Dashboard</h1>
+        <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+          <InfoCard title="Total clients" value="6389">
+            <RoundIcon
+              icon={PeopleIcon}
+              iconColorClass="text-orange-500 dark:text-orange-100"
+              bgColorClass="bg-orange-100 dark:bg-orange-500"
+              className="mr-4"
+            />
+          </InfoCard>
+
+          <InfoCard title="Account balance" value="$ 46,760.89">
+            <RoundIcon
+              icon={MoneyIcon}
+              iconColorClass="text-green-500 dark:text-green-100"
+              bgColorClass="bg-green-100 dark:bg-green-500"
+              className="mr-4"
+            />
+          </InfoCard>
+
+          <InfoCard title="New sales" value="376">
+            <RoundIcon
+              icon={CartIcon}
+              iconColorClass="text-blue-500 dark:text-blue-100"
+              bgColorClass="bg-blue-100 dark:bg-blue-500"
+              className="mr-4"
+            />
+          </InfoCard>
+
+          <InfoCard title="Pending contacts" value="35">
+            <RoundIcon
+              icon={ChatIcon}
+              iconColorClass="text-teal-500 dark:text-teal-100"
+              bgColorClass="bg-teal-100 dark:bg-teal-500"
+              className="mr-4"
+            />
+          </InfoCard>
+        </div>
+      </Main>
+    </>
   );
 };
 
